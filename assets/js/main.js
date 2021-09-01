d0f2b04bf5dea4db0aaeeb93f3c57e253e9b11a2
Guuzzeji <46883697+Guuzzeji@users.noreply.github.com>
2021-09-01 12:33:19 -0700

    Main Files Upload :)

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var typing = new Typed('#typed', {
+    strings: ['<strong>Hi, I’m Gabe (Aka Guuzzeji)</strong>'],
+    typeSpeed: 80,
+    backSpeed: 0,
+    cursorChar: "",
+    loop: false
+});
+
+
+
